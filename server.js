61648c9e6c32d84a2c6922e0e5e537183cb9f9a7
Vladislav <vladplotnikov34@gmail.com>
2017-07-29 18:13:57 +0300

    Version 1.5 development of tracking

--- server.js
+++ server.js
@@ -20,13 +20,9 @@ con.connect(function(err) {
 });
 /*-----Конец блока подкдючения к sql------*/
 
-/*----Блок подключения непосредственно к нашей базе данных по назанию orders :
-  "use orders" - указываем что работаем с этой базой данных----------*/
-con.query("use recievedmail", function (err, result) {
-    if (err) throw err;
-    console.log("use ok"); // выводим сообщение в консоль, что все хорошо  и к базе данных подключилось
-  });
-/*----Конец блока подключения----------*/
+
+
+
 
 /*------реакция на get запрос. Обращаемся к самому сайту и послыаем пользователю нашу верстку.
 Делаем это чтобы на сайте прогрузилась вся наша верстка------*/
@@ -46,10 +42,23 @@ app.get("/adress", function(req,res){
   res.setHeader("Content-Type","text/html");
   res.sendFile(__dirname +"/adress.html");
 })
+
+app.get("/tracking", function(req,res){
+  res.setHeader("Content-Type","text/html");
+  res.sendFile(__dirname +"/tracking.html");
+})
 /*------Конец этого блока------*/
 
-/*-----Реализуем получение данных о клиенете с сайта (форм для ввода) на сервак. А с сервака идет в sql базу данных-----*/
+/*-----Реализуем получение данных о сообщение клиента с сайта (форм для ввода) на сервак. А с сервака идет в sql базу данных-----*/
 app.post("/sendMail", function(req,res){
+  /*----Блок подключения непосредственно к нашей базе данных по назанию recievedmail :
+    "use recievedmail" - указываем что работаем с этой базой данных----------*/
+  con.query("use recievedmail", function (err, result) {
+      if (err) throw err;
+      console.log("use ok"); // выводим сообщение в консоль, что все хорошо  и к базе данных подключилось
+    });
+  /*----Конец блока подключения----------*/
+
  var number =  req.body.name + req.body.email +req.body.phone + req.body.subject + req.body.message;
  console.log(number);
    /*обращаемся к базе данных, посылая запрос на вставку записис в базу.
@@ -64,6 +73,39 @@ app.post("/sendMail", function(req,res){
 })
 /*------Конец этого блока------*/
 
+
+/*-----Реализуем получение данных о трекинг-номере с сайта (форм для ввода) на сервак. А с сервака идем сравнивать в sql базу данных и ищем таблицу с таким же трекинг номером-----*/
+app.post("/checkTrackingCode", function(req,res){
+
+  var trackingNumber =  req.body.trackingCode; // записали считаный трекинг код в отдельную переменую
+  var answer;
+  console.log(trackingNumber);
+
+  /*----Блок подключения непосредственно к нашей базе данных по назанию tracking_info :
+    "use tracking_info" - указываем что работаем с этой базой данных----------*/
+  con.query("use tracking_info", function (err, result) {
+      if (err) throw err;
+      console.log("use ok"); // выводим сообщение в консоль, что все хорошо  и к базе данных подключилось
+
+      /*обращаемся к базе данных, посылая запрос на считывание инофрмации из таблицы и записи в таблицу на сайте .
+      Сначала прописываем поля, куда вставить, потом непосредственно значения для вставки */
+      // console.log("SELECT * FROM "+ recievedmail+";");
+      con.query("SELECT * FROM "+trackingNumber+";", function (err, result, fields) {
+       if (err) throw err;
+       answer = JSON.stringify(result) ; // Превратили нашу таболицу в формат JSON
+       console.log(result);
+     });
+      res.setHeader('Content-Type', 'application/json');
+      res.send(answer);
+
+    });
+  /*----Конец блока подключения----------*/
+
+
+
+  //  res.sendFile(__dirname +"/tracking.html");
+})
+/*------Конец этого блока------*/
 
 
 
